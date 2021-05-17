c614242993f4afc57311420fb1a1c32bc52d00a9
POYI LEE <s5819874@hotmail.com>
2021-05-17 10:21:28 +0800

    install pug as view engine

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const pug = require('pug')
 
 const server = app.listen(port, () => console.log(`Server is listening on port:${port}!`))
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
+app.get('/', (req, res, next) => {
+  const payload = {
+    pagetitle: 'Home'
+  }
+  res.status(200).render('home', payload)
+})
