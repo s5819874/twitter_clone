a25f940b0a9b3dc7249e47d2def036d473a79342
POYI LEE <s5819874@hotmail.com>
2021-06-02 15:29:58 +0800

    add cropper to previewed image

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,6 @@
+//globals
+let cropper
+
 $("#postTextarea, #replyTextarea").keyup(event => {
   let textBox = $(event.target)
   let value = textBox.val().trim()
@@ -93,7 +96,16 @@ $("#filePhoto").change(function () {
   if (this.files && this.files[0]) {
     const reader = new FileReader()
     reader.onload = (e) => {
-      $("#imagePreview").attr("src", e.target.result)
+      let image = document.getElementById("imagePreview")
+      image.src = e.target.result
+
+      if (cropper !== undefined) cropper.destroy()
+
+      cropper = new Cropper(image, {
+        aspectRatio: 1 / 1,
+        background: false
+
+      })
     }
     reader.readAsDataURL(this.files[0])
   }
